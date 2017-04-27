8506fc10f79eb32dc2c0cb0223b45c050e0f2efd
crzycbit <crzybluebilly@mailinator.com>
2017-04-27 06:43:19 -0500

    C-Bit 0.12.1

---
+++ qa/pull-tester/tests_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# Copyright (c) 2013-2014 The C-Bit Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+BUILDDIR="/home/billy/c-bit-master-verifyonly"
+EXEEXT=".exe"
+
+# These will turn into comments if they were disabled when configuring.
+ENABLE_WALLET=1
+ENABLE_UTILS=1
+ENABLE_BITCOIND=1
+#ENABLE_ZMQ=1
+
+
